375f456fa43033f6884103faded380c688b272a4
Jonathan Pacheco <JonathanPachecoCruz@hotmail.com>
2025-12-08 21:52:51 -0600

    feat: add Message utils class

---
+++ src/net/trashlib/system/selector/wines/utils/MessagesUtils.java
@@ -0,0 +1,19 @@
+package net.trashlib.system.selector.wines.utils;
+
+public class MessagesUtils {
+  private String WELCOME_MESSAGE = "Bienvenido al Sistema Selector de Vinos!";
+  private String MESSAGE_INITIAL = "Elegimos el vino adecuado para tu comida.";
+  private String QUESTION_MESSAGE = "Responde correctamente con: \n(y)es. \n(n)o.";
+
+  public String getWelcomeMessage() {
+    return WELCOME_MESSAGE;
+  }
+
+  public String getMessageInitial() {
+    return MESSAGE_INITIAL;
+  }
+
+  public String getQuestionMessage() {
+    return QUESTION_MESSAGE;
+  }
+}
